dc14c055f68116d7e8a7d79a014cb896f42beb6a
phts99 <phts99@gmail.com>
2022-10-08 17:22:08 -0300

    part(login): Adicionado imports da tela de login

--- CarpeDiem/App.js
+++ CarpeDiem/App.js
@@ -1,6 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import * as AkarIcons from "akar-icons";
+import {
+  View,
+  KeyboardAvoidingView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from "react-native";
 
 export default function App() {
   return (
