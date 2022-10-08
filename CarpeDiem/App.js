951845b13551f2e06f8ef73fe6477b495517f0ef
phts99 <phts99@gmail.com>
2022-10-08 17:25:36 -0300

    part(login): Adicionado ícones das redes sociais Carpe Diem

--- CarpeDiem/App.js
+++ CarpeDiem/App.js
@@ -12,18 +12,45 @@ import {
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.socialMediaIcons}>
+        <AkarIcons.InstagramFill
+          color="#2C9196"
+          strokeWidth={2}
+          size={20}
+          style={{ display: "block" }}
+        />
+        <AkarIcons.TwitterFill
+          color="#2C9196"
+          strokeWidth={2}
+          size={20}
+          style={{ display: "block" }}
+        />
+        <AkarIcons.FacebookFill
+          color="#2C9196"
+          strokeWidth={2}
+          size={20}
+          style={{ display: "block" }}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F1ECE9",
+  },
+  socialMediaIcons: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "right",
+    bottom: "1%",
+    left: "42%",
   },
 });
