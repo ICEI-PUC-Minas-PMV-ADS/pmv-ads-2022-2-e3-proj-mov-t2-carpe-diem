74655de9ff7f0837cd6af95718205f8116401b26
phts99 <phts99@gmail.com>
2022-10-11 16:14:55 -0300

    fix(linha-separadora): Readicionado const de style

--- CarpeDiem/src/shared/componentes-visuais/linha-separadora.js
+++ CarpeDiem/src/shared/componentes-visuais/linha-separadora.js
@@ -1,12 +1,24 @@
 import React from "react";
 import {
   View,
+  StyleSheet,
 } from "react-native";
 
 const LinhaSeparadora = ({ color }) => (
-  <View>
+  <View style={styles.linhaSeparadora}>
     <hr style={{ width: "400px", height: "0px", left: "202px", top: "320px", color: black }} />
   </View>
 );
+
+const styles = StyleSheet.create({
+  linhaSeparadora: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "right",
+    bottom: "1%",
+  },
+});
 
 export {LinhaSeparadora};
