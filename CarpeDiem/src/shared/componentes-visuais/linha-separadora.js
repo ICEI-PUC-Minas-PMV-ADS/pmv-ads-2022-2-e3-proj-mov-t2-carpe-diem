9a8747718a2b425447ffd0ff2fe8d30140b232d5
phts99 <phts99@gmail.com>
2022-10-11 16:04:33 -0300

    feat: Adicionado componente para 'Linha Separadora'

---
+++ CarpeDiem/src/shared/componentes-visuais/linha-separadora.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const LinhaSeparadora = ({ color }) => (
+  <hr style={{ width: "400px", height: "0px", left: "202px", top: "320px" }} />
+);
+
+export {LinhaSeparadora};
