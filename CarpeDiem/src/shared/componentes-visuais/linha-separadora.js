6922b526b479438b77f63a880b94483da056ed85
phts99 <phts99@gmail.com>
2022-10-11 16:07:00 -0300

    style(linha-separadora): Adicionado cor para o style do componente

--- CarpeDiem/src/shared/componentes-visuais/linha-separadora.js
+++ CarpeDiem/src/shared/componentes-visuais/linha-separadora.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 const LinhaSeparadora = ({ color }) => (
-  <hr style={{ width: "400px", height: "0px", left: "202px", top: "320px" }} />
+  <hr style={{ width: "400px", height: "0px", left: "202px", top: "320px", color: black }} />
 );
 
 export {LinhaSeparadora};
