f736cdcb85b0c0d485d76d68575a62a74a602744
phts99 <phts99@gmail.com>
2022-10-11 22:00:17 -0300

    feat(header icons): Adicionado componente de icones do cabeçalho

--- CarpeDiem/src/login/Login.js
+++ CarpeDiem/src/login/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import * as AkarIcons from "akar-icons";
 import {
   View,
   KeyboardAvoidingView,
@@ -12,30 +11,13 @@ import {
 import { LinhaSeparadora } from "../shared/componentes-visuais/linha-separadora";
 import { FooterIcons } from "../shared/footer/footer-icons";
 import { FooterText } from "../shared/footer/footer-text";
+import { HeaderIcons } from "../shared/header/header-icons";
+import * as AkarIcons from "akar-icons";
 
 export default function Login() {
   return (
     <KeyboardAvoidingView style={styles.background}>
-      <View style={styles.socialMediaIcons}>
-        <AkarIcons.InstagramFill
-          color="#2C9196"
-          strokeWidth={2}
-          size={20}
-          style={{ display: "block" }}
-        />
-        <AkarIcons.TwitterFill
-          color="#2C9196"
-          strokeWidth={2}
-          size={20}
-          style={{ display: "block" }}
-        />
-        <AkarIcons.FacebookFill
-          color="#2C9196"
-          strokeWidth={2}
-          size={20}
-          style={{ display: "block" }}
-        />
-      </View>
+      <HeaderIcons/>
 
       <View style={styles.containerLogo}>
         <Image
@@ -170,15 +152,6 @@ const styles = StyleSheet.create({
   },
   esqueceuSenhaText: {
     fontSize: 10,
-  },
-  socialMediaIcons: {
-    paddingVertical: 15,
-    paddingHorizontal: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "right",
-    bottom: "1%",
-    left: "42%",
   },
   socialMediaLoginIcons: {
     flex: 1,
---
+++ CarpeDiem/src/shared/header/header-icons.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import * as AkarIcons from "akar-icons";
+
+function HeaderIcons() {
+  return (
+    <View style={styles.socialMediaIcons}>
+      <AkarIcons.InstagramFill
+        color="#2C9196"
+        strokeWidth={2}
+        size={20}
+        style={{ display: "block" }}
+      />
+      <AkarIcons.TwitterFill
+        color="#2C9196"
+        strokeWidth={2}
+        size={20}
+        style={{ display: "block" }}
+      />
+      <AkarIcons.FacebookFill
+        color="#2C9196"
+        strokeWidth={2}
+        size={20}
+        style={{ display: "block" }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  socialMediaIcons: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "right",
+    bottom: "1%",
+    left: "42%",
+  },
+});
+
+export { HeaderIcons };
