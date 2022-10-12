3f0d891ef1bf9c903784e156af0eacde7a47b220
phts99 <phts99@gmail.com>
2022-10-11 21:39:37 -0300

    feat(footer icons): Adicionado componente de icones do rodape

--- CarpeDiem/src/login/Login.js
+++ CarpeDiem/src/login/Login.js
@@ -11,6 +11,7 @@ import {
   StyleSheet,
 } from "react-native";
 import { LinhaSeparadora } from "../shared/componentes-visuais/linha-separadora";
+import { FooterIcons } from "../shared/footer/footer-icons";
 
 export default function Login() {
   return (
@@ -105,62 +106,7 @@ export default function Login() {
 
       <LinhaSeparadora/>
 
-      <View style={styles.footerIcons}>
-        <View style={styles.footerIconsIndividual}>
-          <Icon name="home" size={20} color="#2C9196" />
-          <Text
-            style={{
-              fontSize: 7,
-            }}
-          >
-            Busque por um imóvel
-          </Text>
-        </View>
-
-        <View style={styles.footerIconsIndividual}>
-          <Icon name="phone" size={20} color="#2C9196" />
-          <Text
-            style={{
-              fontSize: 7,
-            }}
-          >
-            Fale conosco
-          </Text>
-        </View>
-
-        <View style={styles.footerIconsIndividual}>
-          <Icon name="info-circle" size={20} color="#2C9196" />
-          <Text
-            style={{
-              fontSize: 7,
-            }}
-          >
-            Políticas e Condições de Uso
-          </Text>
-        </View>
-
-        <View style={styles.footerIconsIndividual}>
-          <Icon name="user-plus" size={20} color="#2C9196" />
-          <Text
-            style={{
-              fontSize: 7,
-            }}
-          >
-            Cadastre-se
-          </Text>
-        </View>
-
-        <View style={styles.footerIconsIndividual}>
-          <Icon name="user-circle" size={20} color="#2C9196" />
-          <Text
-            style={{
-              fontSize: 7,
-            }}
-          >
-            Perfil
-          </Text>
-        </View>
-      </View>
+      <FooterIcons/>
 
       <View>
         <Text style={styles.direitosReservadosText}>
@@ -246,19 +192,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     bottom: "1%",
-  },
-  footerIcons: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    bottom: "1%",
-  },
-  footerIconsIndividual: {
-    paddingVertical: 15,
-    paddingHorizontal: 10,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
   },
   footerText: {
     fontSize: 7,
---
+++ CarpeDiem/src/shared/footer/footer-icons.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+function FooterIcons() {
+  return (
+    <View style={styles.footerIcons}>
+      <View style={styles.footerIconsIndividual}>
+        <Icon name="home" size={20} color="#2C9196" />
+        <Text
+          style={{
+            fontSize: 7,
+          }}
+        >
+          Busque por um imóvel
+        </Text>
+      </View>
+
+      <View style={styles.footerIconsIndividual}>
+        <Icon name="phone" size={20} color="#2C9196" />
+        <Text
+          style={{
+            fontSize: 7,
+          }}
+        >
+          Fale conosco
+        </Text>
+      </View>
+
+      <View style={styles.footerIconsIndividual}>
+        <Icon name="info-circle" size={20} color="#2C9196" />
+        <Text
+          style={{
+            fontSize: 7,
+          }}
+        >
+          Políticas e Condições de Uso
+        </Text>
+      </View>
+
+      <View style={styles.footerIconsIndividual}>
+        <Icon name="user-plus" size={20} color="#2C9196" />
+        <Text
+          style={{
+            fontSize: 7,
+          }}
+        >
+          Cadastre-se
+        </Text>
+      </View>
+
+      <View style={styles.footerIconsIndividual}>
+        <Icon name="user-circle" size={20} color="#2C9196" />
+        <Text
+          style={{
+            fontSize: 7,
+          }}
+        >
+          Perfil
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  footerIcons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    bottom: "1%",
+  },
+  footerIconsIndividual: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+
+export { FooterIcons };
