3fee7664863380922b0be8351fb6720f837f3288
phts99 <phts99@gmail.com>
2022-10-29 13:25:15 -0300

    part(custom metro): Adicionado config. customizada parcial

--- CarpeDiem/metro.config.js
+++ CarpeDiem/metro.config.js
@@ -1,4 +1,13 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
+// const { getDefaultConfig } = require('expo/metro-config');
+
+// module.exports = getDefaultConfig(__dirname);
+
+
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push('db');
+
+module.exports = defaultConfig;
