452692f6bf19d16d84231c467497a9cae2b01e5f
phts99 <phts99@gmail.com>
2022-10-30 19:28:22 -0300

    feat(BD): Adicionado implementação inicial SQLite

---
+++ CarpeDiem/src/database/services/SQLExecutor.js
@@ -0,0 +1,19 @@
+import Database from "./DbServices";
+
+const DB_EXEC = Database.getConnection();
+
+//Seleciona todos usuarios
+export const getUsuario = async () => {
+  let results = await DB_EXEC(`select * from usuario`);
+  console.log(results.rows._array);
+  return results.rows._array;
+};
+
+//Insere usuario
+export const insertUsuario = async (param) => {
+  let results = await DB_EXEC(`insert into usuario (id, nome, cpf, email, senha) values(?,?,?,?,?)`,
+    [param.id, param.nome, param.cpf, param.email, param.senha]
+  );
+  console.log(results.rowsAffected);
+  return results.rowsAffected;
+};
