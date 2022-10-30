d7ce97bd04f9e5d7ce6bd9e244c8335fd346d63f
phts99 <phts99@gmail.com>
2022-10-30 14:19:39 -0300

    fix(linha separado): Adicionado componente <hr> nativo

--- CarpeDiem/src/shared/components/visuais/linha-separadora.js
+++ CarpeDiem/src/shared/components/visuais/linha-separadora.js
@@ -1,23 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 
-const LinhaSeparadora = ({ color }) => (
-  // <View style={styles.linhaSeparadora}>
-  //   <hr style={{ width: "400px", height: "0px", left: "202px", top: "320px"}} />
-  // </View>
-
-  <Text style={styles.linhaSeparadora}>LINHA SEPARADORA </Text>
-);
+const LinhaSeparadora = () => <View style={styles.linhaSeparadora} />;
 
 const styles = StyleSheet.create({
   linhaSeparadora: {
-    //paddingVertical: 15,
-    //paddingHorizontal: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    bottom: "1%",
-    padding: 10,
+    borderBottomColor: "black",
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
 
