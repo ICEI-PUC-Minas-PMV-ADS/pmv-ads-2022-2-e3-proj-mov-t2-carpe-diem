e634209e6d991b4bbedb19be7b9a2664fe551e82
phts99 <phts99@gmail.com>
2022-11-12 20:09:22 -0300

    chore(warning logs): Removido logs de warning da visão do usuário

--- CarpeDiem/index.js
+++ CarpeDiem/index.js
@@ -1,6 +1,9 @@
 import { registerRootComponent } from "expo";
+import { LogBox } from "react-native"
 
 import App from "./App";
+
+LogBox.ignoreAllLogs(true)
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
