7bcf50ff54d4724a958f54acadacebef0a310ab4
phts99 <phts99@gmail.com>
2022-11-26 17:02:42 -0300

    refactor(imports): Removido useEffect inutilizado

--- CarpeDiem/src/screens/obrigado/Obrigado.js
+++ CarpeDiem/src/screens/obrigado/Obrigado.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { StyleSheet, Text, SafeAreaView, ScrollView, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
